860b33ab4168153a56d024174285a48dd103655f
Diana Kleban <dik548@g.harvard.edu>
2017-10-06 14:18:54 -0500

    Added Book and Welcome Controller and changed routes

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        return 'Show all books';
+    }
+
+    public function show($title)
+    {
+        return 'Your book tilte is: ' . $title;
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WelcomeController');
+
+Route::get('/books', 'BookController@index');
+Route::get('/book/{title}', 'BookController@show');
